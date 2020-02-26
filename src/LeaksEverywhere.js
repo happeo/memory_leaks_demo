2069593f274e754e324bb50f3ad2efff694ccf1a
Vlad Mirel <vladmirel@Vlads-MacBook-Pro.local>
2020-02-26 10:21:17 +0200

    back to original state

--- src/LeaksEverywhere.js
+++ src/LeaksEverywhere.js
@@ -1,5 +1,4 @@
 import React from "react";
-import makeCancelable from "./promiseHelper";
 
 class LeaksEverywhere extends React.Component {
   constructor(props) {
@@ -18,20 +17,7 @@ class LeaksEverywhere extends React.Component {
   };
 
   componentDidMount = () => {
-    this.cancelablePromise = makeCancelable(fetch("www.happeo.com"));
-
-    this.cancelablePromise.promise
-      .then(() => {
-        this.someMethod();
-        this.setState({
-          text: "but you can join Happeo and make the world better/happier"
-        });
-      })
-      .catch(err => err.isCanceled && console.log("all good here"));
-  };
-
-  componentWillUnmount = () => {
-    this.cancelablePromise.cancel();
+    window.addEventListener("resize", this.someMethod, false);
   };
 
   render = () => <div>{this.state.text}</div>;
